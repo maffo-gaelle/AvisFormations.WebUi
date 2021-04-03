964f3309c4964d1e08652f2ba704aa6821d7df86
Maffo Melagui Aurelie <gaellemaffo@yahoo.fr>
2021-04-03 03:36:00 +0200

    first commit

---
+++ avisFormations.WebUi/Controllers/FormationController.cs
@@ -0,0 +1,20 @@
+﻿using avisFormations.WebUi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace avisFormations.WebUi.Controllers
+{
+    public class FormationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new ToutesLesFormationsViewModel();
+            vm.Message = "Ceci est un message du controlleur";
+            vm.Nombre = 93;
+            return View(vm);
+        }
+    }
+}
